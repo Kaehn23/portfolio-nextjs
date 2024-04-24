d6b6214b534aa3656776c8beb18ef4500763d608
Jeremy Ipa <jeremy.iparraguirre@gmail.com>
2024-04-24 17:41:45 +0200

    test SEO 1.2

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import Main from "@/components/Main";
 import About from "@/components/About";
@@ -8,28 +8,40 @@ import Contact from "@/components/Contact";
 
 export default function Home() {
    return (
-      <div>
-         <Head>
-           <title>Portfolio de Jeremy - Développeur Web</title>
-           <meta name="description" content="Découvrez le portfolio de Jeremy, développeur web, incluant ses compétences, projets, et informations de contact." />
-           <meta name="keywords" content="j-idev, informatique, Jeremy, iparraguirre, jeremy iparraguirre, développeur, developpeur, freelance, site vitrine, développeur web, portfolio, projets, web, compétences en programmation, contact développeur" />
-           <meta name="robots" content="index, follow" />
-           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-           <meta name="author" content="Jeremy" />
-           <meta name="copyright" content="Copyright © 2023 Jeremy" />
-         </Head>
-         <header>
-           <Navbar />
-         </header>
-         <main>
-           <Main />
-           <About />
-           <Skills />
-           <Projects />
-         </main>
-         <footer>
-           <Contact />
-         </footer>
-      </div>
+      <body>
+         
+            <Head>
+               <title>Portfolio de Jeremy - Développeur Web</title>
+               <meta
+                  name="description"
+                  content="Découvrez le portfolio de Jeremy, développeur web, incluant ses compétences, projets, et informations de contact."
+               />
+               <meta
+                  name="keywords"
+                  content="j-idev, informatique, Jeremy, iparraguirre, jeremy iparraguirre, développeur, developpeur, freelance, site vitrine, développeur web, portfolio, projets, web, compétences en programmation, contact développeur"
+               />
+               <meta name="robots" content="index, follow" />
+               <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1.0"
+               />
+               <meta name="author" content="Jeremy" />
+               <meta name="copyright" content="Copyright © 2023 Jeremy" />
+            </Head>
+
+            <header>
+               <Navbar />
+            </header>
+            <main>
+               <Main />
+               <About />
+               <Skills />
+               <Projects />
+            </main>
+            <footer>
+               <Contact />
+            </footer>
+         
+      </body>
    );
 }
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,59 +1,75 @@
-'use client'
+"use client";
+import Head from "next/head";
 import React from "react";
 import Link from "next/link";
 import { AiOutlineMail } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Main = () => {
-
-    const handleMailClick = () => {
-        window.location.href = "mailto:jeremy.iparraguirre@gmail.com";
-    };
+   const handleMailClick = () => {
+      window.location.href = "mailto:jeremy.iparraguirre@gmail.com";
+   };
 
    return (
-      <div id="home" className="w-full h-screen text-center">
-         <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-            <div>
-               <p className="uppercase text-sm tracking-widest text-gray-600 my-10">
-                  Construisons ensemble
-               </p>
-               {/* Image de profil */}
-               <img
-                  src="/assets/profilePic1.png"
-                  alt="Votre Nom"
-                  className="rounded-full shadow-xl shadow-gray-800 hover:scale-110 ease-in duration-500 mx-auto my-4"
-                  style={{ maxWidth: "200px", maxHeight: "200px" }}
-               />
-               <h1 className="py-4 text-gray-600">
-                  {" "}
-                  Hey, je suis  <span className="text-[#9ebb5f]"> Jeremy.</span>
-               </h1>
-               <h1 className=" text-gray-600">Un développeur full stack !</h1>
-               <p className="py-4 text-gray-500 max-w-[70] m-auto">
-                  Je suis passionné par le développement web & mobile, et toujours à la recherche de nouveaux challenges et opportunités !  
-               </p>
-               <a className="py-2 text-2xl underline " href="resume">
-                  Consultez mon CV !
-               </a>
-               <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-                  <Link href="https://www.linkedin.com/in/iparraguirre-jeremy-3b391a2a3/">
-                     <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                        <FaLinkedinIn />
+      <>
+         <Head>
+            <title>Accueil - Jeremy, Développeur Full Stack</title>
+            <meta
+               name="description"
+               content="Bienvenue sur le portfolio de Jeremy, un développeur full stack passionné par le développement web et mobile. Découvrez mes projets et compétences."
+            />
+         </Head>
+         <div id="home" className="w-full h-screen text-center">
+            <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+               <div>
+                  <p className="uppercase text-sm tracking-widest text-gray-600 my-10">
+                     Construisons ensemble
+                  </p>
+                  {/* Image de profil */}
+                  <img
+                     src="/assets/profilePic1.png"
+                     alt="Votre Nom"
+                     className="rounded-full shadow-xl shadow-gray-800 hover:scale-110 ease-in duration-500 mx-auto my-4"
+                     style={{ maxWidth: "200px", maxHeight: "200px" }}
+                  />
+                  <h1 className="py-4 text-gray-600">
+                     {" "}
+                     Hey, je suis{" "}
+                     <span className="text-[#9ebb5f]"> Jeremy.</span>
+                  </h1>
+                  <h1 className=" text-gray-600">
+                     Un développeur full stack !
+                  </h1>
+                  <p className="py-4 text-gray-500 max-w-[70] m-auto">
+                     Je suis passionné par le développement web & mobile, et
+                     toujours à la recherche de nouveaux challenges et
+                     opportunités !
+                  </p>
+                  <a className="py-2 text-2xl underline " href="resume">
+                     Consultez mon CV !
+                  </a>
+                  <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+                     <Link href="https://www.linkedin.com/in/iparraguirre-jeremy-3b391a2a3/">
+                        <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                           <FaLinkedinIn />
+                        </div>
+                     </Link>
+                     <Link href="https://github.com/Kaehn23">
+                        <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                           <FaGithub />
+                        </div>
+                     </Link>
+                     <div
+                        className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300"
+                        onClick={handleMailClick}
+                     >
+                        <AiOutlineMail />
                      </div>
-                  </Link>
-                  <Link href="https://github.com/Kaehn23">
-                     <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                        <FaGithub />
-                     </div>
-                  </Link>
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300"
-                  onClick={handleMailClick}>
-                     <AiOutlineMail />
                   </div>
                </div>
             </div>
          </div>
-      </div>
+      </>
    );
 };
 
